757257ee9872039b2f99ab2fd31002fae81d7c75
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-25 00:40:31 +0300

    add state to Cild A

--- src/components/ChildA.jsx
+++ src/components/ChildA.jsx
@@ -1,11 +1,14 @@
-import React from 'react';
+import {useState} from "react";
 
 const ChildA = () => {
-    return (
-        <div>
-            
-        </div>
-    );
-}
+  const [number, setNumber] = useState(0);
+  return (
+    <div>
+      <h3>Child A</h3>
+      <p>{number}</p>
+      <button onClick={() => setNumber((number) => number + 1)}>+</button>
+    </div>
+  );
+};
 
 export default ChildA;
