6b8e9606e67ec4a7625d2ecb5ffcf1b73b5a7443
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-26 02:04:05 +0300

    add jsx with props to project

--- src/components/ChildB.jsx
+++ src/components/ChildB.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 
-const ChildB = ({ number }) => {
+const ChildB = ({ number, text }) => {
   return (
     <div style={{ border: "2px solid black", padding: "10px", margin: "20px" }}>
       <h3>Child B</h3>
       <p>{number}</p>
+      {text}
     </div>
   );
 };
--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -4,12 +4,18 @@ import ChildB from "./ChildB";
 
 const Parent = () => {
   const [number, setNumber] = useState(0);
+  const clickHandler = () => {
+    setNumber((number) => number + 1);
+  };
   return (
     <div style={{ border: "2px solid black", padding: "10px", margin: "20px" }}>
       <h3>Parent</h3>
       <button onClick={() => setNumber((number) => number + 1)}>+</button>
-      <ChildA number={number} setNumber={setNumber}/>
-      <ChildB number={number} text={<button onClick={clickHandler}>+</button>} />
+      <ChildA number={number} setNumber={setNumber} />
+      <ChildB
+        number={number}
+        text={<button onClick={clickHandler}>+</button>}
+      />
     </div>
   );
 };
