49ce8de87acae88dfdf7ed5fc046f7a4389bd17d
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-27 15:20:49 +0300

    pass all jsx with props to StyledDiv

--- src/components/Parent.jsx
+++ src/components/Parent.jsx
@@ -10,11 +10,15 @@ const Parent = () => {
   };
   return (
     <div>
-      <StyledDiv />
-      <h3>Parent</h3>
-      <button onClick={() => setNumber((number) => number + 1)}>+</button>
-      <ChildA number={number} setNumber={setNumber} />
-      <ChildB number={number} button={<Button clickHandler={clickHandler} />} />
+      <StyledDiv>
+        <h3>Parent</h3>
+        <button onClick={() => setNumber((number) => number + 1)}>+</button>
+        <ChildA number={number} setNumber={setNumber} />
+        <ChildB
+          number={number}
+          button={<Button clickHandler={clickHandler} />}
+        />
+      </StyledDiv>
     </div>
   );
 };
