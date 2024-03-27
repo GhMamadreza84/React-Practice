f4d3bc15db76c987989015f59ca211b4160af2c9
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-27 15:15:45 +0300

    delete styles from ChildA component

--- src/components/ChildA.jsx
+++ src/components/ChildA.jsx
@@ -2,7 +2,7 @@ import { useState } from "react";
 
 const ChildA = ({ number, setNumber }) => {
   return (
-    <div style={{ border: "2px solid black", padding: "10px", margin: "20px" }}>
+    <div>
       <h3>Child A</h3>
       <p>{number}</p>
       <button onClick={() => setNumber((number) => number + 1)}>+</button>
