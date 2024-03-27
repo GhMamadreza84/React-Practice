628febf3a15645917c5c02ca2db362807cc7947d
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-27 15:26:36 +0300

    use props with children

--- src/components/StyledDiv.jsx
+++ src/components/StyledDiv.jsx
@@ -1,9 +1,9 @@
 
-const StyledDiv = () => {
+const StyledDiv = ({children}) => {
     const styles = {border: "2px solid black", padding: "10px", margin: "20px"}
     return (
         <div style={styles}>
-            
+            {children}
         </div>
     );
 }
