1e1291ec3627553bbb81be5a601bfde4311b16ab
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-27 15:16:14 +0300

    delete styles from ChildB component

--- src/components/ChildB.jsx
+++ src/components/ChildB.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const ChildB = ({ number, button }) => {
   return (
-    <div style={{ border: "2px solid black", padding: "10px", margin: "20px" }}>
+    <div>
       <h3>Child B</h3>
       <p>{number}</p>
       {button}
