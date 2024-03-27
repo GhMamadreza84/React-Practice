590c2183fc602eaee5e87e50474c43601f1547c5
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-03-27 15:27:30 +0300

    add styleddiv to Child A

--- src/components/ChildA.jsx
+++ src/components/ChildA.jsx
@@ -1,12 +1,13 @@
 import { useState } from "react";
+import StyledDiv from "./StyledDiv";
 
 const ChildA = ({ number, setNumber }) => {
   return (
-    <div>
+    <StyledDiv>
       <h3>Child A</h3>
       <p>{number}</p>
       <button onClick={() => setNumber((number) => number + 1)}>+</button>
-    </div>
+    </StyledDiv>
   );
 };
 
