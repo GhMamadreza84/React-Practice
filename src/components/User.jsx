c70194299ecb340e19d1b9d54fc58eebe5155ae9
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-03 23:22:18 +0300

    send request to api with useEffect

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,10 +1,13 @@
 import { useEffect } from "react";
 const User = () => {
-    
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((res) => res.json())
+      .then((json) => console.log(json));
+  }, []);
   return (
     <div>
       <p>User</p>
-
     </div>
   );
 };
