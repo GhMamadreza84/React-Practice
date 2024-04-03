2edc2c1139e3117c18d0bf1d088579c70fe125c2
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-03 23:11:50 +0300

    test again life cycles

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -1,12 +1,16 @@
-
 const User = () => {
-    return (
-        <div>
-            <p>User</p>
+  return (
+    <div>
+      <p>User</p>
+
+      {/*
             // Life Cycle
-            // 1 - Mounting 2 - Updating 3 - UnMounting
-        </div>
-    );
-}
+            // 1 - Mounting : First Render
+            // 2 - Updating : Rerender - State - Props
+            // 3 - UnMounting : Delete Items 
+            */}
+    </div>
+  );
+};
 
 export default User;
