2f18b797d0995ee447ce1964ccc3a7d4bc9f93e6
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-03 23:12:22 +0300

    remove life cycle test

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -3,12 +3,6 @@ const User = () => {
     <div>
       <p>User</p>
 
-      {/*
-            // Life Cycle
-            // 1 - Mounting : First Render
-            // 2 - Updating : Rerender - State - Props
-            // 3 - UnMounting : Delete Items 
-            */}
     </div>
   );
 };
