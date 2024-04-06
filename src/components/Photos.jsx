62aa7856120d4ce64e363cdf9ec9e4563b3d75d3
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-06 21:54:07 +0200

    edit if in phtos

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -4,7 +4,7 @@ const Photos = () => {
   const [isShow, setIsShow] = useState(true);
   return (
     <div>
-      {isShow ? <h1>Botostart</h1> : <h1>React course</h1>}
+      {isShow && <h1>React course</h1>}
       <button onClick={() => setIsShow((isShow) => !isShow)}>toggle</button>
     </div>
   );
