5609852e79de0b9b9f16be49bee6248a91481db8
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-06 21:54:16 +0200

    add title to photos

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -4,6 +4,7 @@ const Photos = () => {
   const [isShow, setIsShow] = useState(true);
   return (
     <div>
+        <h1>Botostart</h1> 
       {isShow && <h1>React course</h1>}
       <button onClick={() => setIsShow((isShow) => !isShow)}>toggle</button>
     </div>
