ee7a9be5aecdc7f02d8fc720c22e8e3ef847216c
Gh Mamadreza <ghmamadreza84@gmail.com>
2024-04-06 21:50:14 +0200

    add structure photos component

--- src/components/Photos.jsx
+++ src/components/Photos.jsx
@@ -1,11 +1,13 @@
-import React from 'react';
+import React from "react";
 
 const Photos = () => {
-    return (
-        <div>
-            photos
-        </div>
-    );
-}
+  return (
+    <div>
+      <h1>Botostart</h1>
+      <h3>React course</h3>
+      <button>toggle</button>
+    </div>
+  );
+};
 
 export default Photos;
